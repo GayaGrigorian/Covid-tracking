4f43d482f1bf0a3502dc095ccd893b3e93c17092
Gayane Grigoryan <102904157+gaya94@users.noreply.github.com>
2022-11-07 15:15:20 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Covid from './Covid';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+       <Header/>
        <Covid/>
     </div>
   );
