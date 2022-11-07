a33509bec8f1a40f7a14e2fc3061ecbd502c5217
Gayane Grigoryan <102904157+gaya94@users.noreply.github.com>
2022-11-07 15:17:10 +0400

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <div className="Header">    
+                <div className="Title">Date</div>
+                <div className="Title">State</div>
+                <div className="Title">Positive</div>
+                <div className="Title">Death</div>
+                <div className="Title">Recovered</div>
+            </div>
+    )
+} 
